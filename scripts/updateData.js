43c106066dec6b398f576b28384fece0b6f14e6c
Justas Palumickas <jpalumickas@gmail.com>
2019-10-21 17:37:50 +0300

    Exclude src from list

--- scripts/updateData.js
+++ scripts/updateData.js
@@ -1,8 +1,13 @@
 const fs = require('fs').promises;
 const { all } = require('known-css-properties');
 const camelCaseCss = require('camelcase-css');
+const excludedList = ['src'];
 
 (async () => {
-  const data = all.map(item => camelCaseCss(item)).sort();
+  const data = all
+    .map(item => camelCaseCss(item))
+    .filter(item => !excludedList.includes(item))
+    .sort();
+
   await fs.writeFile(`${__dirname}/../src/inlineProperties.json`, JSON.stringify(data));
 })();

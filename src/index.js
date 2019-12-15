daced13d406cb37d7e956c5ea2f5c305323ace9e
Justas Palumickas <jpalumickas@gmail.com>
2019-12-15 12:14:14 +0200

    Add Prettier

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import memoize from 'mem';
-import inlineProperties from './inlineProperties.json'
+import inlineProperties from './inlineProperties.json';
 
 const fn = (key) => inlineProperties.includes(key);
 const hasProperty = memoize(fn);
 
-export {
-  hasProperty,
-}
+export { hasProperty };
 
 export default inlineProperties;
